4dd97929d4bd88fa79edb71bffeda46e64daff43
schnuffler32u4 <104348927+schnuffler32u4@users.noreply.github.com>
2023-02-01 03:14:07 +0100

    Implemented a basic maxima counting algorithm

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import statistics
 plt.style.use('extensys')
 
 wl = 650e-9  # the wavelength of the lase in m
-n = 1.51  # index of refraction of the glass
+# n = 1.51  # index of refraction of the glass
+thickness = 1e-3 # the thickness of one of the glass plates
 
 
 def roundup(x):
@@ -26,20 +27,91 @@ def roundup(x):
         return x
 
 
-def fitfunction(theta, thickness, path, bg):
+def fitfunction(theta, n, path, bg, amp):
+    """Returns the light intensity for a given angle and plate thickness"""
+    theta = theta * (2.9 / 42) ** (-1)
+    return bg + amp * np.sin(((1 - n) * thickness / np.cos(theta) + path) / wl)
+
+
+def fitfunction2(theta, n, path, bg, amp):
     """Returns the light intensity for a given angle and plate thickness"""
     theta = theta * 2.9 / 42
-    return bg + np.sin(((1 - n) * thickness / np.cos(theta) + path) / wl)
+    return bg + amp * np.sin(((1 - n) * 2 * thickness / np.cos(theta) + path) / wl)
 
 
 thick = []
 thickerr = []
+ns = []
 
 for file in os.listdir('data/one_plate'):
     # if file[:4] != "data":
     if True:
         data = pd.read_csv('data/one_plate/' + file)
-    print(file)
+    # print(file)
+    data.rename(columns={"Angle (rad) Run #1": "Angle", "Light Intensity, Ch 1 (lx) Run #1": "Light"}, inplace=True)
+    data.dropna(inplace=True)
+    data.drop(data[data.Light < np.average(data.Light) - 1.3 * (np.amax(data.Light) - np.average(data.Light))].index, inplace=True)
+    data.drop(data[data.Light > np.average(data.Light) + 1.3 * (-np.amin(data.Light) + np.average(data.Light))].index,
+              inplace=True)
+    angle = np.array(data.Angle)
+    angle[0] = 0
+    nans = []
+
+    for i in range(len(angle)):
+        if np.isnan(angle[i]) == False:
+            nans.append(i)
+
+    for j in range(len(nans) - 1):
+        angle[nans[j]:nans[j+1]] = np.linspace(angle[nans[j]], angle[nans[j+1]], len(angle[nans[j]:nans[j+1]]))
+    # print(angle)
+    popt, pcov = curve_fit(fitfunction, angle, data.Light, maxfev=500000, p0=[1.5, 0, np.average(data.Light), np.amax(data.Light) - np.average(data.Light)])
+    cal = np.zeros(len(angle))
+    peaks = np.zeros(len(angle), dtype=int)
+    light = np.array(data.Light)
+    datanew = pd.DataFrame({"Angle": angle, "Light": light})
+    datanew.sort_values(by=['Angle'], ascending=True, inplace=True)
+    angle = np.array(datanew.Angle)
+    light = np.array(datanew.Light)
+
+    fig, ax = plt.subplots()
+    ax.plot(angle, fitfunction(angle, *popt), color='cyan')
+    ax.plot(angle, light, color='red')
+    m = 0
+    for j in range(1,len(light)-1):
+        if light[j - 1] < light[j] > light[j + 1] and np.amin(light[peaks[m-1]:j]) < np.average(light) and sum([1 for x in light[peaks[m-1]:j] if x < np.average(light)]) / len(light[peaks[m-1]:j]) > 0.3 and light[j] > np.average(light) and light[j] > np.average(light) + 0.1 * (np.amax(light) - np.average(light)):
+            #print(yum)
+            peaks[m] = j
+            circle1 = plt.Circle((angle[j], light[j]), 0.05, color='blue')
+            ax.add_patch(circle1)
+            m = m + 1
+    # plt.show()
+    # print(popt[0])
+    thick.append(popt[0])
+    thickerr.append(np.sqrt(pcov[0][0]))
+    thet = np.amax(np.abs([np.amax(angle), np.amin(angle)]) * 2.9 / 42)
+    fringes = sum([1 for x in peaks if x != 0])
+    alph = fringes * wl / (2 * thickness)
+    nny = (alph**2 + 2 * (1-np.cos(thet)) * (1-alph) )/ (2 * (1-np.cos(thet) - alph))
+    ns.append(nny)
+
+print(np.average(nny))
+#thick = np.array(thick)
+#thickerr = np.array(thickerr)
+#print('For one plate the optimal thickness value and the erorr in it (in meters) are given by:')
+#print(np.sum(thick / thickerr ** 2) / np.sum(1 / thickerr ** 2))
+#print(np.sqrt(1 / np.sum(1 / thickerr ** 2)))
+
+
+# We now move on to work on the second type of data collected by the experiment, mainly the one
+
+thick = []
+thickerr = []
+
+for file in os.listdir('data/two_plates'):
+    # if file[:4] != "data":
+    if True:
+        data = pd.read_csv('data/two_plates/' + file)
+    # print(file)
     data.rename(columns={"Angle (rad) Run #1": "Angle", "Light Intensity, Ch 1 (lx) Run #1": "Light"}, inplace=True)
     data.dropna(inplace=True)
     angle = np.array(data.Angle)
@@ -52,9 +124,9 @@ for file in os.listdir('data/one_plate'):
     for j in range(len(nans) - 1):
         angle[nans[j]:nans[j+1]] = np.linspace(angle[nans[j]], angle[nans[j+1]], len(angle[nans[j]:nans[j+1]]))
     # print(angle)
-    popt, pcov = curve_fit(fitfunction, angle, data.Light, maxfev=500000, p0=[1e-3, 0, statistics.median(data.Light)])
+    popt, pcov = curve_fit(fitfunction, angle, data.Light, maxfev=500000, p0=[2e-3, 0, statistics.median(data.Light), np.amax(data.Light) - statistics.median(data.Light)])
 
-    plt.plot(angle, data.Light, color='red')
+    plt.plot(angle, data.Light, color='red', label='Data')
     cal = np.zeros(len(angle))
 
     plt.plot(angle, fitfunction(angle, *popt), color='cyan')
@@ -65,5 +137,6 @@ for file in os.listdir('data/one_plate'):
 
 thick = np.array(thick)
 thickerr = np.array(thickerr)
+print('For two plates the optimal thickness value and the erorr in it (in meters) are given by:')
 print(np.sum(thick / thickerr ** 2) / np.sum(1 / thickerr ** 2))
 print(np.sqrt(1 / np.sum(1 / thickerr ** 2)))
